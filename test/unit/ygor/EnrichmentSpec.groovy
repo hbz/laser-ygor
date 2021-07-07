f3bb2bc7a8e605bfb532e2c7bee64fb0e72b66d8
Philipp von Böselager <philipp.boeselager@web.de>
2021-07-07 17:05:42 +0200

    Add EnrichmentSpec.testCreateEnrichment

--- test/unit/ygor/EnrichmentSpec.groovy
+++ test/unit/ygor/EnrichmentSpec.groovy
@@ -1,7 +1,14 @@
 package ygor
 
+import de.hbznrw.ygor.processing.YgorFeedback
 import grails.test.mixin.TestFor
+import org.apache.commons.io.FileUtils
+import org.junit.Test
 import spock.lang.Specification
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,11 +16,30 @@ import spock.lang.Specification
 @TestFor(Enrichment)
 class EnrichmentSpec extends Specification {
 
+  Path ygorTestBasePath
+  Enrichment enrichment
+
   def setup() {
-    true
+    ygorTestBasePath = Files.createDirectories(Paths.get("/tmp/ygor/test"))
   }
 
+  @Test
+  testCreateEnrichment(){
+    given:
+    String kbartFile = "test/resources/KBart02.tsv"
+    YgorFeedback ygorFeedback = new YgorFeedback(YgorFeedback.YgorProcessingStatus.PREPARATION,
+        "EnrichmentSpec.testCreateEnrichment(). ", this.getClass(), null, null, null, null)
+
+    when:
+    enrichment = new Enrichment(ygorTestBasePath.toFile() ,kbartFile, ygorFeedback)
+
+    then:
+    enrichment.enrichmentFolder.startsWith(ygorTestBasePath.toString())
+  }
+
+
+
   def cleanup() {
-    true
+    FileUtils.deleteDirectory(new File(enrichment.enrichmentFolder))
   }
 }
