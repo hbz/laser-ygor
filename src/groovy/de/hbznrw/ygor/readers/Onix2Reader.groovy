e13dc4d071dc590c9de8532fd0637c0aa356b9b0
Philipp von Böselager <philipp.boeselager@web.de>
2021-07-22 16:02:48 +0200

    Enable node name data extraction without localPart (ERMS-3547)

--- src/groovy/de/hbznrw/ygor/readers/Onix2Reader.groovy
+++ src/groovy/de/hbznrw/ygor/readers/Onix2Reader.groovy
@@ -4,6 +4,7 @@ import groovy.util.logging.Log4j
 import org.codehaus.groovy.grails.plugins.web.taglib.ValidationTagLib
 import org.springframework.web.multipart.commons.CommonsMultipartFile
 
+import javax.annotation.Nonnull
 import java.time.LocalDate
 import ygor.EnrichmentService
 
@@ -74,15 +75,28 @@ class Onix2Reader extends AbstractOnixReader{
     if (fieldValue instanceof Node){
       String prefix = fieldName.equals("") ? "" : fieldName.concat(":")
       fieldValue.eachWithIndex{ def node, int j ->
+        String fieldValueName = getNodeName(fieldValue)
         if (node instanceof Node){
-          readItemEntries(entries, "$prefix${fieldValue.name().localPart}:$i".toString(), node, j)
+          readItemEntries(entries, "$prefix${fieldValueName}:$i".toString(), node, j)
         }
         else{
-          readItemEntries(entries, "$prefix${fieldValue.name().localPart}".toString(), node, j)
+          readItemEntries(entries, "$prefix${fieldValueName}".toString(), node, j)
         }
       }
     }
     return entries
+  }
+
+
+  private String getNodeName(@Nonnull Node node){
+    String nodeName = null
+    try{
+      nodeName = node.name()?.localPart
+    }
+    catch (MissingPropertyException mpe){
+      nodeName = node.name()
+    }
+    return nodeName
   }
 
 
